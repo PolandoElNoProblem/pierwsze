68c1ffa9e2a9f6683aeb92ff439ecb1dfff54ada
dolezinski.jan <dolezinski.jan@STACJA39A-02>
2022-09-19 08:23:58 +0200

    Dodaj pliki projektów.

---
+++ pierwsze/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace pierwsze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ez = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.besttime = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // ez
+            // 
+            this.ez.Enabled = false;
+            this.ez.Font = new System.Drawing.Font("French Script MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ez.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.ez.Location = new System.Drawing.Point(349, 183);
+            this.ez.Name = "ez";
+            this.ez.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ez.Size = new System.Drawing.Size(128, 51);
+            this.ez.TabIndex = 0;
+            this.ez.Text = "(kliknij mnie)";
+            this.ez.UseVisualStyleBackColor = true;
+            this.ez.Click += new System.EventHandler(this.ez_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("French Script MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wynik: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(391, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Czas: 0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Zacznij klikanie!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // besttime
+            // 
+            this.besttime.AutoSize = true;
+            this.besttime.BackColor = System.Drawing.Color.Transparent;
+            this.besttime.Location = new System.Drawing.Point(370, 338);
+            this.besttime.Name = "besttime";
+            this.besttime.Size = new System.Drawing.Size(94, 13);
+            this.besttime.TabIndex = 7;
+            this.besttime.Text = "Najlepszy wynik: 0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.besttime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ez);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kliker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label besttime;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ pierwsze/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pierwsze
+{
+    public partial class Form1 : Form
+    {
+        private int licznik = 0;
+        private int czas = 0;
+        private int a ;
+       private  int c ;
+        private int d ;
+        private int best_time=0 ;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ez_Click(object sender, EventArgs e)
+        {
+            
+            licznik++;
+            c = licznik + d;
+            label1.Text ="Wynik: "+ c.ToString();
+        }
+
+        private void x(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+          a++;
+            d = a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = 0;
+            licznik = 0;
+            czas = 0;
+            timer1.Start();
+
+            timer2.Start();
+            label2.Text = "Czas: " + czas.ToString() + 's';
+            ez.Enabled = true;
+            button1.Enabled = false;
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ez.Enabled = false;
+
+            if (best_time < c)
+            {
+                best_time = c;
+                besttime.Text="Najlepszy wynik: "+c.ToString(); 
+            }
+            button1.Enabled = true;
+            
+            timer2.Stop();
+            timer1.Stop();
+            czas++;
+            label2.Text = "Czas: " + czas.ToString() + 's';
+                
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            czas++;
+            label2.Text = "Czas: "+czas.ToString() + 's';
+        }
+    }
+}
